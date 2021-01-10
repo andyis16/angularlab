8758b28994773f4278251c7a3940ac879d9e40d5
liz <elizabeth.collacott2fcagroup.com>
2021-01-10 10:57:46 -0500

    first commit

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Todo {
+  task: string;
+  completed: boolean;
+}
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+  
+  // create an array of todos, do I need the word public
+
+  todos: Todo[] = [
+    
+    {task: 'fold clothes', completed: true },
+    {task: 'put clothes in drawer', completed: false },
+    {task: 'pet dog', completed: true },
+    {task: 'be awesome', completed: false },
+
+  ]
+
+  // add title
+  title: string = "My ToDo List"
+  comment: string ="A place to store your to-dos"
+  newTodo: string; 
+
+
+
+  // click completed button
+  strikethrough=function (todo): void {
+    todo.completed = true;
+
+  }
+
+  // add task
+
+  addTask=function (todo:string): void {
+    console.log (todo);
+    let newTask = {
+      task: todo, completed: false, 
+    }
+    this.todos.push (newTask)
+    
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
+
+}
+
